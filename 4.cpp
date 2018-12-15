3c647bd1830d61f2e398168c40e6bef46edf8d99
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:29:05 +0700

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int C,R,F;
+
+cout<<"Program konversi suhu"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"masukan nilai celcius:";
+cin>>C;
+R=C*0.8;
+F=C*1.8+32;
+cout<<"nilai Reamur adalah "<<R<<endl;
+cout<<"nilai Fahrenheit adalah "<<F;
+return 0;
+
+}
